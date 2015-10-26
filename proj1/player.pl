8ac02e10aa117a845690e9c10cc9592c2cb8dead
Diogo Marques <xmarques999@hotmail.com>
2015-10-26 13:44:52 +0000

    para melhor entendimento

--- proj1/player.pl
+++ proj1/player.pl
@@ -23,24 +23,26 @@ initializePlayer(Name, NewState):-
 	player(Name),
 	NewState = playerState(Name, 24, 24).
 
-resetPlayer(playerState(Name, _, _), NewState):-
+resetPlayer(playerState(Name, _NumberDiscs, _NumberRings), NewState):-
 	player(Name),
 	NewState = playerState(Name, 24, 24).
 
 getPlayerName(playerState(Name, _NumberDiscs, _NumberRings), Name).
 
-hasRings(playerState(Name, _, NumberRings)):-
+hasRings(playerState(Name, _NumberDiscs, NumberRings)):-
 	player(Name),
 	NumberRings > 0.
 
-hasDiscs(playerState(Name, NumberDiscs, _)):-
+hasDiscs(playerState(Name, NumberDiscs, _NumberRings)):-
 	player(Name),
 	NumberDiscs > 0.
 
-decrementDiscs(playerState(Name, NumberDiscs, _), playerState(Name, NewDiscs, _)):-
+decrementDiscs(playerState(Name, NumberDiscs, _NumberRings), 
+	playerState(Name, NewDiscs, _NumberRings)):-
 	player(Name),
 	NewDiscs is NumberDiscs - 1.
 
-decrementRings(playerState(Name, _, NumberRings), playerState(Name, _, NewRings)):-
+decrementRings(playerState(Name, _NumberDiscs, NumberRings),
+	playerState(Name, _NumberDiscs, NewRings)):-
 	player(Name),
 	NewRings is NumberRings - 1.
